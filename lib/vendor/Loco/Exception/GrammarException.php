33ee0c30621be31d9bad858e59e4ac4e7203d4f3
ju1ius <jules.bernable@laposte.net>
2011-11-17 02:44:45 +0100

    deleted Loco Parser

--- lib/vendor/Loco/Exception/GrammarException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Loco\Exception;
-
-// This occurs at Parser instantiation time, e.g. left-recursion, null-stars,
-// miscellaneous housekeeping errors
-class GrammarException extends \Exception
-{
-
-}
