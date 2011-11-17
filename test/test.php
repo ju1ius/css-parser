3431c32d0cd85673bd3b9df5b5cc7c21ebaae196
ju1ius <jules.bernable@laposte.net>
2011-11-17 03:18:46 +0100

    get rid of homebrew autoloader

--- test/test.php
+++ test/test.php
@@ -1,19 +1,16 @@
 <?php
 require_once 'Benchmark/Timer.php';
 require_once __DIR__.'/../lib/vendor/Opl/Autoloader/GenericLoader.php';
-//require_once __DIR__.'/../lib/vendor/Opl/Autoloader/ClassMapLoader.php';
 
 $loader = new Opl\Autoloader\GenericLoader(__DIR__.'/../lib');
-//$loader = new Opl\Autoloader\ClassMapLoader(__DIR__.'/../lib', __DIR__.'/../lib/cache/autoload.cache');
-//$loader->addNamespace('Loco', __DIR__.'/../lib/vendor');
 $loader->addNamespace('CSS');
 $loader->register();
 
 //$css = file_get_contents(__DIR__.'/files/first.css');
 //$css = file_get_contents(__DIR__.'/files/keyframes.css');
-$css = '#bar.foo , #baz-foo > .babar[href$=".jpg"] {
-  foo: bar;  
-}'; 
+$css = "p{
+  foo: url('foo/bar.png');  
+}";
 //var_dump(substr($css, 419, 100));
 
 $parser = new CSS\Parser(array());
@@ -36,6 +33,6 @@ var_dump($result->getCssText(array(
 //var_dump($result->getRuleList()->getAllValues());
 
 
-//var_dump($result);
+var_dump($result);
 echo $timer->getOutput();
 
