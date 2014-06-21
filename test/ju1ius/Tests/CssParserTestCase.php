9bf098887c043fcf8b2077094c59bd46c757cb4b
ju1ius <jules.bernable@laposte.net>
2014-06-21 10:26:53 +0200

    fixed fixtures files path

--- test/ju1ius/Tests/CssParserTestCase.php
+++ test/ju1ius/Tests/CssParserTestCase.php
@@ -16,16 +16,20 @@ class CssParserTestCase extends \PHPUnit_Framework_TestCase
         $stylesheet_loader,
         $css_parser;
 
+    private $fixtures_dir;
+
+
     public function __construct($name=null, $data=array(), $dataName='')
     {
         $this->lexer = new Lexer();
         $this->css_parser = new Parser($this->lexer);
+        $this->fixtures_dir = __DIR__ . '/../../files';
         parent::__construct($name, $data, $dataName);
     }
 
     public function loadFile($file)
     {
-        return file_get_contents(__DIR__.'/files/'.$file);
+        return file_get_contents($this->fixtures_dir . '/'. $file);
     }
 
     public function createParser($strict=true)
@@ -37,7 +41,7 @@ class CssParserTestCase extends \PHPUnit_Framework_TestCase
 
     public function parseFile($file)
     {
-        $source = Loader::load(__DIR__.'/files/'.$file);
+        $source = Loader::load($this->fixtures_dir . '/' . $file);
         $this->lexer->setSource($source);
         return $this->css_parser->parseStyleSheet();
         //return $this->css_parser->parse($source);
