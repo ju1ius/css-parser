cd435facd50de8b2f17298fbbab7a422947687b1
ju1ius <jules.bernable@laposte.net>
2014-06-21 10:30:00 +0200

    updated autoloading in quicktests

--- test/misc/valuelist.php
+++ test/misc/valuelist.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__.'/../autoload.php';
+
+require_once __DIR__.'/../../vendor/autoload.php';
 use ju1ius\Css;
 
 $lexer = new Css\Lexer();
--- test/perf/parser.php
+++ test/perf/parser.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'Benchmark/Timer.php';
-require_once __DIR__.'/../autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
 
 use ju1ius\Text\Source;
